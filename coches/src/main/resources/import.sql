6fd8d493e66c3a5f7b796caa8aee87c6bd96c1a6
ibiensedev <ibiensedev@gmail.com>
2023-03-09 08:21:20 +0100

    creación de script para inserción de datos de prueba

---
+++ coches/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into coche (matricula, marca, modelo, fecha_matriculacion, bastidor) values ("1234ABC", "Toyota", "Hilux", "2018-08-10", 12345);
+insert into coche (matricula, marca, modelo, fecha_matriculacion, bastidor) values ("4567FGD", "Peugeot", "206", "2008-08-10", 12345);
+insert into coche (matricula, marca, modelo, fecha_matriculacion, bastidor) values ("0987UYT", "BMW", "F800GS", "2015-08-10", 12345);
+insert into coche (matricula, marca, modelo, fecha_matriculacion, bastidor) values ("7645QWE", "Peugeot", "406", "2001-08-10", 12345);
+insert into coche (matricula, marca, modelo, fecha_matriculacion, bastidor) values ("1098FHG", "Suzuki", "Santana", "1988-08-10", 12345);
